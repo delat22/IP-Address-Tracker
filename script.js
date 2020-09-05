7a1597e466783b89440afaf477c5b1aad7a2a2db
delat22 <babalolalateef18@gmai.com>
2020-09-05 17:08:01 +0100

    update js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 //DOM ELEMENTS
-const inputIp = document.getElementById('input');
+const inputValue = document.getElementById('input');
 const submitButton = document.getElementById('btin');
 
 const ipAddress = document.getElementById('address');
@@ -11,87 +11,45 @@ const timezone = document.getElementById('timezone');
 const isp = document.getElementById('isp');
 
 
-//Adding Event Listener
-submitButton.addEventListener('click', ($event) => {
-    $event.preventDefault();
+submitButton.addEventListener('click', function(event) {
 
-    const post = inputIp.value;
-
-    submitFormData(post);
-
-    /*
-    request.open('GET', 'http://ip-api.com/json/'+ inputIp.value +'?fields=9002');
-    request.send();
-    */
+  const inputText = inputValue.Value;
+  
+  var result = fetch(`https://geo.ipify.org/api/v1?apiKey=at_DuE1mQRJGaRuY1kbYgW6ggKTBGMDw&ipAddress=${inputText}`);
+  
+  result.then(response => {
+      return response.json();
+  }).then(data => {
+      var ipAddressRes = `${data.ip}`;
+      var locationRes = `${data.location.region}, ${data.location.country} ${data.location.postalCode}`;
+      var timeZoneRes = `UTC ${data.location.timezone}`;
+      var ispRes = `${data.isp}`;
+      //var latitudeRes = `${data.location.lat}`;
+      //var longitudeRes = `${data.location.lng}`;
+      ipAddress.innerHTML = ipAddressRes;
+      location.textContent = locationRes;
+      timezone.innerHTML = timeZoneRes;
+      isp.innerHTML = ispRes;
+     // myMap.panTo(new L.LatLng(latitudeRes, longitudeRes));
+      //L.marker([latitudeRes, longitudeRes], {icon: mapMarker}).addTo(myMap);
+  });
 });
 
-
-// An example script for redirecting users from USA to https://google.com/
-// and users from Canada to https://google.ca/
-
-// ip-api endpoint URL
-// we need only the countryCode, but you can request more fields
-// see http://ip-api.com/docs/api:json for documentation
-/** 
-function makeRequest(data){
-   var endpoint = `http://ip-api.com/json/${data}`;
-
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(this.responseText);
-      if(response.status !== 'success') {
-        console.log('query failed: ' + response.message);
-        return
-      }
-          // Redirect
-        
-      ipAddress.textContent= response.query,
-          location.textContent =  response.regionName + response.countryCode + response.zip,
-        timezone.textContent =  response.timezone,
-          isp.textContent =  response.isp
-    }
-  };
-  xhr.open('GET', endpoint, true);
-  xhr.send();
-
+/*
+{
+  "status": "success",
+  "country": "Nigeria",
+  "countryCode": "NG",
+  "region": "LA",
+  "regionName": "Lagos",
+  "city": "Ikoyi",
+  "zip": "",
+  "lat": 6.44368,
+  "lon": 3.42653,
+  "timezone": "Africa/Lagos",
+  "isp": "MTN NIGERIA Communication limited",
+  "org": "Staticnatfor Gprswimaxcorporate",
+  "as": "AS29465 MTN NIGERIA Communication limited",
+  "query": "197.210.227.103"
 }
-
 */
-//Fetch API
-
-function makeRequest(data) {
-    return new Promise((resolve, reject) => {
-      let request = new XMLHttpRequest();
-      request.open('GET', `http://ip-api.com/json/${data}`);
-      request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-          if (request.status === 201) {
-            resolve(JSON.parse(request.response));
-          } else {
-            reject(JSON.parse(request.response));
-          }
-        }
-      };
-      request.send();
-    });
-  }
-
-
-  //Receive API response
-
-  async function submitFormData(post) {
-      try{
-        const requestPromise = makeRequest(post);
-        const response = await requestPromise;
-        ipAddress.textContent = response.query;
-        location.textContent =  response.regionName + response.countryCode + response.zip;
-        timezone.textContent =  response.timezone;
-        isp.textContent =  response.isp;
-      }
-
-      catch (errorResponse) { 
-
-        alert('query failed: ' + response.message);
-      }
-  }
